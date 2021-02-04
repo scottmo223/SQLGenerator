4a66ea3c629b54931c2ac3989688cb421bae79b2
scottmo223 <morales.scott@gmail.com>
2021-02-04 11:39:42 -0600

    rough outline of program flow

--- SQLGenerator.py
+++ SQLGenerator.py
@@ -1,2 +1,14 @@
+import csv
+
+#set file to variable
 newValues = 'newValues.csv'
 
+#open file 
+
+#save file contents to object
+
+#Set up sql string template
+
+#insert file object contents into string template
+
+#save sql to new file
