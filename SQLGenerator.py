5650a9a985bff40c0b4c2e55cdf5b6692c33fa54
scottmo223 <morales.scott@gmail.com>
2021-02-04 12:25:04 -0600

    adds file reader and file contents object

--- SQLGenerator.py
+++ SQLGenerator.py
@@ -1,11 +1,16 @@
 import csv
 
 #set file to variable
-newValues = 'newValues.csv'
+newValuesFile = 'newValues.csv'
 
 #open file 
-
+with open(newValuesFile, newline='') as csvfile:
 #save file contents to object
+    reader = csv.DictReader(csvfile)
+
+    for row in reader:
+        print(row['id'], row['questiontext'], row['newquestiontext'])
+
 
 #Set up sql string template
 
